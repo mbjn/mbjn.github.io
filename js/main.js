5237b0522f69df8e091893ba1312206a44c6aa42
mbjn <81683048+mbjn@users.noreply.github.com>
2021-08-08 17:10:54 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use script'
 {
+// タブページに関するJS
   document.addEventListener('DOMContentLoaded', function(){
     // タブに対してクリックイベントを適用
     const tabs = document.getElementsByClassName('tab');
@@ -20,8 +21,45 @@
     };  
   });
 
+//Anotherスクリーンに関するjs 
+// panel-ano のスクリーンとボタンの切り替え
+//ボタンクリックで同じsrcの切り替え
+
+const btns = document.querySelectorAll('anobtn');
+
+btns.forEach(btn, () => {
+  const line = `${btn.index} - ${btn.dataset.id}`;
+  console.log(line);
+} );
 
 
+
+// btns.forEach(btn,index => { 
+//   const btns = document.querySelectorAll('anobtn');
+//   const describes  = document.querySelectorAll('desc > p');
+ 
+//   const screen = document.getElementsByClassName('screen');
+//   const desc = document.getElementsByClassName('desc');
+  
+//   btns.addEventListener('click', () =>{
+//     let i = document.getElementById(clickedItem.dataset.id)-1;
+//     let zu = imgesId[i];
+   
+//     // // この次にimg と説明文を 付け替える
+//     // screen.removeChild;
+//     // describe.removeChild;
+    
+//     // //要素の作成
+//     // const btnImg = document.createElement('img');
+//     // const btndesc = document.createElement('p');
+
+//     // //要素の中身の作成と親要素に追加
+//     // screen.add(btnImg.getAttribute('src', zu));
+//     // descrive.add(btndesc.textContent(descId[i]));
+    
+//   });
+  
+// });
 
 
   
