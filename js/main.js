b2d44c7aa010768ac42dd4d4f0c4616ef58a84a5
mbjn <81683048+mbjn@users.noreply.github.com>
2021-08-10 02:25:45 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
-'use script'
+'use strict'
+
 {
-// タブページに関するJS
+
+// タブで画面の切り替え
+
   document.addEventListener('DOMContentLoaded', function(){
-    // タブに対してクリックイベントを適用
+    // タブにクリックイベントを付加
     const tabs = document.getElementsByClassName('tab');
     for(let i = 0; i < tabs.length; i++) {
       tabs[i].addEventListener('click', tabSwitch);
     }
   
-    // タブをクリックすると実行する関数
+    // タブクリック関数
     function tabSwitch(){
-      // タブのclassの値を変更
+      // タブのclassを変更
       document.getElementsByClassName('is-active')[0].classList.remove('is-active');
       this.classList.add('is-active');
-      // コンテンツのclassの値を変更
+      // コンテンツのclassを変更
       document.getElementsByClassName('is-show')[0].classList.remove('is-show');
       const arrayTabs = Array.prototype.slice.call(tabs);
       const index = arrayTabs.indexOf(this);
@@ -21,47 +24,47 @@
     };  
   });
 
-//Anotherスクリーンに関するjs 
-// panel-ano のスクリーンとボタンの切り替え
-//ボタンクリックで同じsrcの切り替え
 
-const btns = document.querySelectorAll('anobtn');
+// ボタンで画面の切り替え
 
-btns.forEach(btn, () => {
-  const line = `${btn.index} - ${btn.dataset.id}`;
-  console.log(line);
-} );
+const btns = document.querySelectorAll('button');
 
+const imgs = [
+   "img/m1.jpg",
+   "img/m2.jpg",
+   "img/m3.jpg",
+   "img/m4.jpg",
+   "img/m5.jpg",
+  ];
 
+// const shows =[
+//     ".show1",
+//     ".show2",
+//     ".show3",
+//     ".show4",
+//     ".show5",
+//   ];
+// console.log(shows);
 
-// btns.forEach(btn,index => { 
-//   const btns = document.querySelectorAll('anobtn');
-//   const describes  = document.querySelectorAll('desc > p');
- 
-//   const screen = document.getElementsByClassName('screen');
-//   const desc = document.getElementsByClassName('desc');
+const x0 = document.getElementById('x0');
+
+btns.forEach( (btn,index) => {
+  const i = index ;
+  console.log(`btn: ${btn.textContent}-${i}`);
   
-//   btns.addEventListener('click', () =>{
-//     let i = document.getElementById(clickedItem.dataset.id)-1;
-//     let zu = imgesId[i];
-   
-//     // // この次にimg と説明文を 付け替える
-//     // screen.removeChild;
-//     // describe.removeChild;
+  btn.addEventListener( 'click', e => {
+    e.preventDefault;
     
-//     // //要素の作成
-//     // const btnImg = document.createElement('img');
-//     // const btndesc = document.createElement('p');
-
-//     // //要素の中身の作成と親要素に追加
-//     // screen.add(btnImg.getAttribute('src', zu));
-//     // descrive.add(btndesc.textContent(descId[i]));
+    x0.setAttribute('src', imgs[i]);
     
-//   });
-  
-// });
+    const shows = document.querySelectorAll('.show');
+    shows.forEach(show  => {
+      show.classList.remove('active');
+    });
+    shows[i].classList.add('active');
 
+  });
 
-  
+});
 
 }
